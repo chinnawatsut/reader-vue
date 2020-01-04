352cfcd4c67bcc109c98c21edc0abecfd8e1dd7d
chinwat kotchum <chinwat@odd.works>
2020-01-04 11:49:04 +0700

    02_component_pt2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="home">
-    <h1>Hello {{ name }}</h1>
-    <input type="text" v-model="name" />
-    <div>
-      <img src="https://cdn.bitdegree.org/learn/pexels-photo-920220.jpeg?4d5e638c" :alt="name" />
-    </div>
-    <ul>
-      <li v-for="(book,index) in books" :key="index">{{book.name}}</li>
+  <div class="container">
+    <h2>Total: {{ books.length }}</h2>
+    <h2>HighScore: {{ highScoreCount }}</h2>
+    <h2>HighCount: {{ highCount}}</h2>
+    <h2>highScore: {{ highScore() }}</h2>
+
+    <h3>LowScore: {{ books.length - highScoreCount }}</h3>
+    <ul class="list-group">
+      <li
+        v-for="(book,index) in books"
+        :key="index"
+        :class="{ 'highlight': book.score > 6 }"
+        class="list-group-item"
+      >{{book.name}} ({{book.score}}/10)</li>
     </ul>
-    <button v-on:click="toggleShow"> show/hide </button>
-    <div v-if="isShow">Hi vIf</div>
-    <div v-show="isShow">Hi vShow</div>
+    <form class @submit.prevent="createBook">
+      <div class="form-group row">
+        <label for="name">Name</label>
+        <input class="form-control" id="name" v-model="name" type="text" />
+      </div>
+      <div class="form-group row">
+        <label for="score">Score</label>
+        <select class="form-control" v-model="score" name="score" id="score">
+          <option v-for="(score,index) in 10" :value="score" :key="index">{{ score}}</option>
+        </select>
+      </div>
+      <button class="btn btn-primary" type="submit">create</button>
+    </form>
   </div>
 </template>
 
@@ -20,18 +36,44 @@ export default {
   components: {},
   data() {
     return {
-      name: "Smith",
       books: [
         { name: "Harry Potter", score: 7 },
         { name: "Percy", score: 9 },
         { name: "Titanic", score: 6 }
       ],
-      isShow: false
+      name: "",
+      score: 10,
+      highCount: 0
     };
   },
   methods: {
-    toggleShow() {
-      this.isShow = !this.isShow
+    createBook() {
+      const book = {
+        name: this.name,
+        score: this.score
+      };
+      this.books.push(book);
+      this.name = "";
+      this.score = 10;
+    },
+    highScore() {
+      console.log("method: highScore");
+      return this.books.filter(b => b.score > 6).length;
+    }
+  },
+  computed: {
+    highScoreCount() {
+      console.log("computed: highScoreCount");
+      return this.books.filter(b => b.score > 6).length;
+    }
+  },
+  watch: {
+    books: {
+      immediate: true,
+      handler() {
+        console.log("watch: books");
+        this.highCount = this.books.filter(b => b.score > 6).length;
+      }
     }
   }
 };
@@ -41,5 +83,11 @@ export default {
 img {
   width: 50px;
   height: 50px;
+}
+.name {
+  color: brown;
+}
+.highlight {
+  color: #41b883;
 }
 </style>
