139511c7095f19ecc96f9caab3991ad6b0dc49bd
chinwat kotchum <chinwat@odd.works>
2020-01-04 17:23:07 +0700

    logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,34 +1,44 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark d-flex flex-row-reverse"> 
-      <span class="profile">Hi! {{ profile.display }}</span>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-end">
+      <template v-if="profile">
+        <span class="profile">Hi! {{ profile.display }}</span>
+        <button @click="logout" class="btn btn-primary">Logout</button>
+      </template>
+      <template v-else>
+        <button class="btn btn-primary">Register</button>
+      </template>
     </nav>
   </div>
 </template>
 
 <script>
-import eventBus from '../eventBus'
+import eventBus from "../eventBus";
+import localStorage from "../services/localStorage";
 export default {
   data() {
     return {
-      profile: {
-        display: '',
-      }
-    }
+      profile: null
+    };
   },
   mounted() {
-    eventBus.$on('on-signed-in', this.onSignedIn)
+    eventBus.$on("on-signed-in", this.onSignedIn);
   },
   methods: {
     onSignedIn(profile) {
-      this.profile = profile
+      this.profile = profile;
+    },
+    logout() {
+      localStorage.removeToken();
+      location.reload();
     }
   }
 };
 </script>
 
-<style scoped> 
+<style scoped>
 .profile {
   color: aliceblue;
+  margin-right: 20px;
 }
 </style>
--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -2,8 +2,10 @@ const authKey = "token"
 
 const getToken = () => localStorage.getItem(authKey)
 const setToken = (token) => localStorage.setItem(authKey, token)
+const removeToken = () => localStorage.removeItem(authKey)
 
 export default {
   getToken,
-  setToken
+  setToken,
+  removeToken
 }
